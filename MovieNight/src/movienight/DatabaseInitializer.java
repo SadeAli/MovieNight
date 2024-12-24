86a47993bc09ecc7ff0a931e6ac5f6518e5acf03
Oğuzhan Esgiyusufo <77408077+EsgiJ@users.noreply.github.com>
2024-12-24 16:32:19 +0300

    Creating Tables
    Used "User" because User is reserved word. Succesfully created the tables

---
+++ MovieNight/src/movienight/DatabaseInitializer.java
@@ -0,0 +1,62 @@
+package movienight;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class DatabaseInitializer {
+
+	public static void initialize(Connection connection) {
+		
+		try(Statement stmt = connection.createStatement()){
+			
+			String createTables = """
+				CREATE TABLE IF NOT EXISTS "User"(
+                    id SERIAL PRIMARY KEY,
+                    fname VARCHAR(50),
+                    lname VARCHAR(50),
+                    username VARCHAR(50) UNIQUE,
+                    password VARCHAR(50),
+                    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                );
+				
+				CREATE TABLE IF NOT EXISTS Movie(
+					id SERIAL PRIMARY KEY,
+					title VARCHAR(100),
+					description TEXT,
+					trailerPath VARCHAR(200)
+				);
+				
+				CREATE TABLE IF NOT EXISTS Lobby(
+					id SERIAL PRIMARY KEY,
+					owner_id INTEGER REFERENCES "User"(id),
+					date DATE
+				);
+				
+				CREATE TABLE IF NOT EXISTS Suggestion(
+					lobby_id INTEGER REFERENCES Lobby(id),
+					suggested_by INTEGER REFERENCES "User"(id),
+					movie_id INTEGER REFERENCES Movie(id),
+					PRIMARY KEY (lobby_id, movie_id)
+				);
+				
+				CREATE TABLE IF NOT EXISTS Vote(
+					lobby_id INTEGER REFERENCES Lobby(id),
+					user_id INTEGER REFERENCES "User"(id),
+					movie_id INTEGER REFERENCES Movie(id),
+					PRIMARY KEY (lobby_id, user_id, movie_id)
+				);
+				
+				CREATE TABLE IF NOT EXISTS Invitation (
+					sender_id INTEGER REFERENCES "User"(id),
+					lobby_id INTEGER REFERENCES Lobby(id),
+					receiver_id INTEGER REFERENCES "User"(id),
+					PRIMARY KEY (sender_id, receiver_id, lobby_id)
+				);
+			""";
+			stmt.execute(createTables);
+			System.out.println("Tables created successfully!.");
+		} catch (Exception e) {
+			System.err.println("Hata: " + e.getMessage());
+		}
+	}
+}
--- MovieNight/src/movienight/Main.java
+++ MovieNight/src/movienight/Main.java
@@ -10,6 +10,9 @@ public class Main {
             Connection connection = DatabaseConnection.connect();
             System.out.println("Successfully connected to the MovieNight_DB!");
             
+            // Initialize the database (create tables)
+            DatabaseInitializer.initialize(connection);
+            
             // Close the connection
             connection.close();
         } catch (SQLException e) {
