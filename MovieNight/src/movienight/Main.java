8d3fa87b0fa490510e278da3df81cd29f6cefbc9
degD <den.ege.der@gmail.com>
2024-12-30 15:26:34 +0300

    add HomeGUI and LobbyGUI from NetBeans

--- MovieNight/src/movienight/Main.java
+++ MovieNight/src/movienight/Main.java
@@ -3,6 +3,7 @@ package movienight;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 	public static void main(String[] args) {
@@ -11,10 +12,12 @@ public class Main {
             System.out.println("Successfully connected to the MovieNight_DB!");
             
             // Initialize the database (create tables)
-            DatabaseInitializer.initialize(connection);
+            // DatabaseInitializer.initialize(connection);
             
-            // Close the connection
-            connection.close();
+            Statement stmt = connection.createStatement();
+            while (true) {
+            	stmt.execute("Select * from movie");
+            }
         } catch (SQLException e) {
             System.out.println("Connection error: " + e.getMessage());
         }
